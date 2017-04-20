2e16cd64aa29f1f0631db65b649a112e1db16738
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2017-04-20 22:03:50 +1200

    add file writer

--- src/main/java/com/github/sadikovi/riff/FileWriter.java
+++ src/main/java/com/github/sadikovi/riff/FileWriter.java
@@ -24,14 +24,22 @@ package com.github.sadikovi.riff;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Random;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileAlreadyExistsException;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import org.apache.spark.sql.catalyst.InternalRow;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.sadikovi.riff.io.OutputBuffer;
+
 class FileWriter {
+  private static final Logger LOG = LoggerFactory.getLogger(FileWriter.class);
   // suffix for data files
   private static final String DATA_FILE_SUFFIX = ".data";
 
@@ -43,20 +51,30 @@ class FileWriter {
   private Path dataPath;
   // type description for schema to write
   private TypeDescription td;
+  // unique id for this writer, assigned per file
+  private byte[] fileId;
+  // whether or not this file writer has created file already
+  private boolean hasWrittenData;
+  // number of rows in stripe
+  private int numRowsInStripe;
 
   /**
    * Create file writer for path.
+   * Configuration is passed separately and not reused from `fs.getConf`. This is to be explicit
+   * about separate configuration from most of the hadoop settings. Actual user-facing API will
+   * allow providing configuration for both file system and internal options.
    * @param fs file system to use
-   * @param path path to the header file, data path is created from it
+   * @param conf configuration
+   * @param path path to the header file, also used to create data path
+   * @param td type description for rows
    * @throws IOException
    * @throws FileAlreadyExistsException
    */
-  FileWriter(FileSystem fs, Path path, TypeDescription td) throws IOException {
+  FileWriter(FileSystem fs, Configuration conf, Path path, TypeDescription td) throws IOException {
     this.fs = fs;
     this.headerPath = fs.makeQualified(path);
     this.dataPath = makeDataPath(headerPath);
-    this.td = td;
-
+    this.hasWrittenData = false;
     if (this.fs.exists(headerPath)) {
       throw new FileAlreadyExistsException("Already exists: " + headerPath);
     }
@@ -64,6 +82,18 @@ class FileWriter {
       throw new FileAlreadyExistsException(
         "Data path already exists: " + dataPath + ". Data path is created from provided path " +
         path + " and also should not exist when " + "creating writer");
+    }
+    // this assumes that subsequent rows are provided for this schema
+    this.td = td;
+    // generate unique id for this file, collisions are possible, this mainly to prevent accidental
+    // renaming of files
+    this.fileId = nextFileKey();
+
+    this.numRowsInStripe =
+      conf.getInt(Riff.Options.RIFF_STRIPE_ROWS, Riff.Options.RIFF_STRIPE_ROWS_DEFAULT);
+    if (numRowsInStripe < 1) {
+      throw new IllegalArgumentException(
+        "Expected positive number of rows in stripe, found " + numRowsInStripe);
     }
   }
 
@@ -74,6 +104,18 @@ class FileWriter {
    */
   static Path makeDataPath(Path path) {
     return path.suffix(DATA_FILE_SUFFIX);
+  }
+
+  /**
+   * Generate new file key of 12 bytes for file identifier.
+   * This key is shared between header and data files.
+   * @return array with random byte values
+   */
+  static byte[] nextFileKey() {
+    Random rand = new Random();
+    byte[] key = new byte[12];
+    rand.nextBytes(key);
+    return key;
   }
 
   /**
@@ -92,7 +134,27 @@ class FileWriter {
     return dataPath;
   }
 
-  public void writeRows(Iterator<InternalRow> iter) {
+  /**
+   * Create header and data files and write rows.
+   * This method invokes collection of any statistics or filters relevant to the file and writes
+   * stripes of data into file.
+   * @param iter iterator of Spark internal rows
+   */
+  public void writeFile(Iterator<InternalRow> iter) throws IOException {
+    // assert if this file writer has written data already, and fail if so - we do not allow to
+    // reuse instances for multiple data writes.
+    if (hasWrittenData) {
+      throw new IOException("No reuse of file writer " + this);
+    }
+    hasWrittenData = true;
+
+    // first, we write data file and collect all stripe information in order to store it in header
+    // data header consists of 16 bytes - 4 bytes magic and 12 bytes unique key
+    OutputBuffer dataHeader = new OutputBuffer();
+    dataHeader.writeBytes(Riff.MAGIC.getBytes());
+    dataHeader.writeBytes(fileId);
+    assert dataHeader.bytesWritten() == 16: "Invalid number of bytes written - expected 16, got " +
+      dataHeader.bytesWritten();
     // TODO: implement writing rows
   }
 
