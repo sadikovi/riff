0010f8cd53983eafc396628393f6f61e770dd062
Ivan <ivan.sadikov@gmail.com>
2017-05-27 12:38:40 +1000

    Remove IsNotNull tree node for null-safe filters (#68)
    * remove isnotnull node when null-safe exists
    
    * add filter references
    
    * fix reference check
    
    * remove constraints from sequence

--- sql/src/main/scala/com/github/sadikovi/spark/riff/Filters.scala
+++ sql/src/main/scala/com/github/sadikovi/spark/riff/Filters.scala
@@ -31,6 +31,28 @@ import com.github.sadikovi.riff.tree.FilterApi._
  * Utility functions to convert Spark SQL filters into Riff filters.
  */
 private[riff] object Filters {
+
+  /**
+   * Create new Riff filter from sequence of SQL filters.
+   * Sequence should be collapsed as conjunction.
+   * @param filters sequence of SQL filters
+   * @return predicate tree or null
+   *
+   */
+  def createRiffFilter(filters: Seq[Filter]): Tree = {
+    // collect all references for the top level leaf nodes
+    val refs = filters.flatMap { filter =>
+      if (isLeaf(filter) && !isNullRelated(filter)) references(filter) else Nil
+    }.toSet
+    // remove constraint predicates such as IsNotNull from the top level filters
+    // keep IsNotNull that are not part of constraint propagation
+    val noContraintFilters = filters.collect {
+      case filter if !filter.isInstanceOf[IsNotNull] => filter
+      case nonConstraint @ IsNotNull(attr) if !refs.contains(attr) => nonConstraint
+    }
+    createRiffFilter(noContraintFilters.reduceOption(And))
+  }
+
   /**
    * Create new Riff filter for SQL filter.
    * @param filter optional filter
@@ -71,11 +93,21 @@ private[riff] object Filters {
       case IsNotNull(attribute: String) =>
         not(nvl(attribute))
       case And(left: Filter, right: Filter) =>
-        // TODO: Remove Not(IsNull) tree node when there exists an equality for that column
-        // e.g. (!(col1[0] is null)) && (col1[0] > 100). This is how Spark pushes down predicate to
-        // source and adds filters that we do not need to evaluate since our filters are already
-        // null safe.
-        and(recurBuild(left), recurBuild(right))
+        // Remove IsNotNull tree node when there exists a leaf node for that column e.g.
+        // (!(col1[0] is null)) && (col1[0] > 100). Spark adds IsNotNull that can be removed in
+        // file format, since riff filters are always null safe.
+        if (isLeaf(left) && isLeaf(right) && references(left) == references(right)) {
+          if (left.isInstanceOf[IsNotNull] && !isNullRelated(right)) {
+            recurBuild(right)
+          } else if (right.isInstanceOf[IsNotNull] && !isNullRelated(left)) {
+            recurBuild(left)
+          } else {
+            // cannot match filters, apply default transformation
+            and(recurBuild(left), recurBuild(right))
+          }
+        } else {
+          and(recurBuild(left), recurBuild(right))
+        }
       case Or(left: Filter, right: Filter) =>
         or(recurBuild(left), recurBuild(right))
       case Not(child: Filter) =>
@@ -88,5 +120,43 @@ private[riff] object Filters {
         // - StringContains
         TRUE
     }
+  }
+
+  /** Find references for tree node, park 2.0 does not have references for filters */
+  def references(value: Filter): Seq[String] = value match {
+    case EqualTo(attribute, _) => Seq(attribute)
+    case EqualNullSafe(attribute, _) => Seq(attribute)
+    case GreaterThan(attribute, _) => Seq(attribute)
+    case GreaterThanOrEqual(attribute, _) => Seq(attribute)
+    case LessThan(attribute, _) => Seq(attribute)
+    case LessThanOrEqual(attribute, _) => Seq(attribute)
+    case In(attribute, _) => Seq(attribute)
+    case IsNull(attribute) => Seq(attribute)
+    case IsNotNull(attribute) => Seq(attribute)
+    case StringStartsWith(attribute, _) => Seq(attribute)
+    case StringEndsWith(attribute, _) => Seq(attribute)
+    case StringContains(attribute, _) => Seq(attribute)
+    case And(left: Filter, right: Filter) => references(left) ++ references(right)
+    case Or(left: Filter, right: Filter) => references(left) ++ references(right)
+    case Not(child: Filter) => references(child)
+    case _ => Seq.empty
+  }
+
+  /**
+   * Determine if filter is leaf tree ndoe, e.g. not a logical filter.
+   * Normally has only one reference.
+   */
+  def isLeaf(filter: Filter): Boolean = {
+    !filter.isInstanceOf[And] &&
+    !filter.isInstanceOf[Or] &&
+    !filter.isInstanceOf[Not]
+  }
+
+  /**
+   * Whether or not this filter is null related. Currently only includes IsNull and IsNotNull.
+   * Mainly because we cannot apply optimizations when both filters exist in conjunction.
+   */
+  def isNullRelated(filter: Filter): Boolean = {
+    filter.isInstanceOf[IsNull] || filter.isInstanceOf[IsNotNull]
   }
 }
